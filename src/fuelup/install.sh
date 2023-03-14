e61b9ca3a974556d793fac40b5037bd69e480ed9
Sophie <sophiedankel@gmail.com>
2023-03-13 17:28:10 -0700

    apt-get update

--- src/fuelup/install.sh
+++ src/fuelup/install.sh
@@ -1,16 +1,15 @@
 #!/bin/sh
-set -e
+set -x
 
 echo "Installing fuelup (latest toolchain)"
 
-echo "which apt-get"
-echo $(which apt-get)
+which apt-get
 
 echo "apt-get install curl"
-echo $(apt-get install curl)
+apt-get update
+apt-get -qq -y install curl
 
-echo "which curl"
-echo $(which curl)
+which curl
 
 curl --proto '=https' --tlsv1.2 -sSf https://install.fuel.network/fuelup-init.sh | sh -s -- --no-modify-path
 export PATH="${HOME}/.fuelup/bin:${PATH}"
