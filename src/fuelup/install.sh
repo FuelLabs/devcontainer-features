243fc845a15f28efd98f79ef03c86ed0995da627
Sophie <sophiedankel@gmail.com>
2023-03-13 17:15:01 -0700

    more debugging

--- src/fuelup/install.sh
+++ src/fuelup/install.sh
@@ -3,5 +3,9 @@ set -e
 
 echo "Installing fuelup (latest toolchain)"
 
+
+echo "which curl"
+echo $(which curl)
+
 curl --proto '=https' --tlsv1.2 -sSf https://install.fuel.network/fuelup-init.sh | sh -s -- --no-modify-path
 export PATH="${HOME}/.fuelup/bin:${PATH}"
