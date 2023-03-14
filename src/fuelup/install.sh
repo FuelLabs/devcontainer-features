d34997f65e281bbabcdbbdbe2d70d6c23c1f6ac5
Sophie <sophiedankel@gmail.com>
2023-03-13 19:06:14 -0700

    Fix hang

--- src/fuelup/install.sh
+++ src/fuelup/install.sh
@@ -3,8 +3,12 @@ set -x
 
 echo "Installing fuelup (latest toolchain)"
 
+# Install curl and git. Unfortunately the devcontainer features don't seem to work, so we
+# have to install them manually.
 apt-get update
-apt-get -qq -y install curl
+
+# These variables are needed to avoid tzdata asking for a timezone on ubuntu.
+DEBIAN_FRONTEND=noninteractive TZ=Etc/UTC apt-get -qq -y install curl
 apt-get -qq -y install git-all
 
 curl --proto '=https' --tlsv1.2 -sSf https://install.fuel.network/fuelup-init.sh | sh -s -- --no-modify-path
