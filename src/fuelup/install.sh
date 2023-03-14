18b69292dc8c8aa2410d89ddf591f49e1b6a0ce1
Sophie <sophiedankel@gmail.com>
2023-03-14 15:21:39 -0700

    Exactly like the example

--- src/fuelup/install.sh
+++ src/fuelup/install.sh
@@ -20,7 +20,6 @@ curl --proto '=https' --tlsv1.2 -sSf https://install.fuel.network/fuelup-init.sh
 # The latest toolchain is installed by default. If a specific toolchain is requested, install it and
 # set it as the default.
 if [ "${TOOLCHAIN}" != "latest" ]; then
-    export PATH="${PATH}:${HOME}/.fuelup/bin"
-    fuelup toolchain install ${TOOLCHAIN}
-    fuelup default ${TOOLCHAIN}
+    $HOME/.fuelup/bin/fuelup toolchain install ${TOOLCHAIN}
+    $HOME/.fuelup/bin/fuelup default ${TOOLCHAIN}
 fi
