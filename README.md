a3430ad60010b1e65d747c709afc22a70a1957eb
Sophie <sophiedankel@gmail.com>
2023-03-14 14:18:45 -0700

    More scenarios and update readme

--- README.md
+++ README.md
@@ -10,13 +10,34 @@ This repository contains a _collection_ of one Feature - `fuelup`.
 
 ### `fuelup`
 
-Running `fuelup` inside the built container will install `fuelup` and the `latest` version of the Fuel toolchain.
+Running `fuelup` inside the built container will install `fuelup` and the specified version of the Fuel toolchain, defaulting to `latest` if none is specified. The options for toolchains are:
+
+- latest
+- beta-1
+- beta-2
+- beta-3
+- nightly
 
 ```jsonc
 {
     "image": "mcr.microsoft.com/devcontainers/base:ubuntu",
     "features": {
-        "ghcr.io/FuelLabs/devcontainer-features/fuelup:1": {}
+        "ghcr.io/FuelLabs/devcontainer-features/fuelup:1": {
+            "toolchain": "latest"
+        }
+    }
+}
+```
+
+is equivalent to:
+
+```jsonc
+{
+    "image": "mcr.microsoft.com/devcontainers/base:ubuntu",
+    "features": {
+        "ghcr.io/FuelLabs/devcontainer-features/fuelup:1": {
+            "toolchain": "latest"
+        }
     }
 }
 ```
---
+++ test/fuelup/beta-2.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+set -x
+
+# This test file will be executed against one of the scenarios devcontainer.json test that
+# includes no options.
+#
+# These scripts are run as 'root' by default. Although that can be changed
+# with the '--remote-user' flag.
+# 
+# This test can be run with the following command:
+#
+#    devcontainer features test \ 
+#                   --features fuelup   \
+#                   --remote-user root \
+#                   --skip-scenarios   \
+#                   --base-image mcr.microsoft.com/devcontainers/base:ubuntu \
+#                   /path/to/this/repo
+
+# Optional: Import test library bundled with the devcontainer CLI
+source dev-container-features-test-lib
+
+# Feature-specific tests
+# The 'check' command comes from the dev-container-features-test-lib.
+check "execute command" which fuelup | grep '.fuelup/bin/fuelup'
+check "execute command" which forc | grep '.fuelup/bin/fuelup'
+check "execute command" fuelup default | grep 'beta-2'
+
+# Report results
+# If any of the checks above exited with a non-zero exit code, the test will fail.
+reportResults
