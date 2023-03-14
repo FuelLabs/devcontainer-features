800249fa938b630a0f8d54744f79bf14b236d570
Sophie <sophiedankel@gmail.com>
2023-03-14 12:24:05 -0700

    export env vars

--- src/fuelup/install.sh
+++ src/fuelup/install.sh
@@ -3,12 +3,15 @@ set -x
 
 echo "Installing fuelup (latest toolchain)"
 
+# These variables are needed to avoid tzdata asking for a timezone on ubuntu.
+export DEBIAN_FRONTEND=noninteractive
+export TZ=Etc/UTC
+
 # Install curl and git. Unfortunately the devcontainer features don't seem to work, so we
 # have to install them manually.
 apt-get update
-
-# These variables are needed to avoid tzdata asking for a timezone on ubuntu.
-DEBIAN_FRONTEND=noninteractive TZ=Etc/UTC apt-get -qq -y install curl
+apt-get -qq -y install curl
 apt-get -qq -y install git-all
 
+# Install fuelup.
 curl --proto '=https' --tlsv1.2 -sSf https://install.fuel.network/fuelup-init.sh | sh -s -- --no-modify-path
