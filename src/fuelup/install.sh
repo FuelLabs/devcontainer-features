296b4a1521d608ecb455853e84ae3b502381df92
Sophie <sophiedankel@gmail.com>
2023-03-14 14:00:44 -0700

    Add option to specify toolchain

--- src/fuelup/install.sh
+++ src/fuelup/install.sh
@@ -1,7 +1,15 @@
 #!/bin/sh
 set -x
 
-echo "Installing fuelup (latest toolchain)"
+echo "Installing fuelup with provided toolchain: ${TOOLCHAIN}"
 
-# Install fuelup.
+# Install fuelup with `--no-modify-path`. The PATH is modified in `devcontainer-feature.json` instead
+# so that it persists in the final image.
 curl --proto '=https' --tlsv1.2 -sSf https://install.fuel.network/fuelup-init.sh | sh -s -- --no-modify-path
+
+# The latest toolchain is installed by default. If a specific toolchain is requested, install it and
+# set it as the default.
+if [ "${TOOLCHAIN}" != "latest" ]; then
+   fuelup toolchain install ${TOOLCHAIN}
+   fuelup default ${TOOLCHAIN}
+fi
