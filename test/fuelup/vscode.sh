7fbbd1165fbd0d0606d592b2fe6d5ddb53b27ff3
Sophie <sophiedankel@gmail.com>
2023-03-17 15:02:18 -0700

    cleanup

--- test/fuelup/vscode.sh
+++ test/fuelup/vscode.sh
@@ -9,7 +9,4 @@ check "execute command" fuelup --version | grep -E 'fuelup (\d+\.)+\d+'
 check "execute command" forc --version | grep -E 'forc (\d+\.)+\d+'
 check "execute command" fuelup default | grep 'latest'
 
-# Test that this non-root user has permission to install new toolchains.
-check "execute command" fuelup toolchain install nightly
-
 reportResults
